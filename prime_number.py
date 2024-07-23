2fdce841cd48e1be4e2e0ce7d916b879556f9ac3
Lovely Gupta <lovelygupta016@gmail.com>
2024-07-23 16:52:15 +0530

    remove dubliate from list

--- prime_number.py
+++ prime_number.py
@@ -9,3 +9,15 @@ def is_prime(n):
     
 n = 13
 print(is_prime(n))
+
+
+# find dbulicate
+
+l = [1,2,3,44,55,55,66,7]
+result = []
+
+for i in l:
+    if i not in result:
+        result.append(i)
+
+print(result)
