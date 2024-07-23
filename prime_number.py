edb29cfcd2ee5745b3acfe97649ef66559f19b09
Lovely Gupta <lovelygupta016@gmail.com>
2024-07-23 15:54:41 +0530

    to check prime number

---
+++ prime_number.py
@@ -0,0 +1,11 @@
+def is_prime(n):
+    if n <=1:
+        return True
+    for i in range(2,n):
+        if n%i == 0:
+            return False
+    else:
+        return True
+    
+n = 12
+print(is_prime(n))
